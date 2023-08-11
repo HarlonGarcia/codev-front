7a38c38e4471c4011e52743156c6cf1ebbf9047c
Harlon Garcia <harloongarcia@gmail.com>
2023-08-10 21:13:39 -0300

    implement technologies feature

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import { commanderReducer } from './features/commanderSlice';
 import { usersReducer } from './features/userSlice';
 import { challengesReducer } from './features/challengeSlice';
 import { categoriesReducer } from './features/categorySlice';
+import { technologiesReducer } from './features/technologySlice';
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ export const store = configureStore({
     users: usersReducer,
     challenges: challengesReducer,
     categories: categoriesReducer,
+    technologies: technologiesReducer,
   },
 });
 
---
+++ src/types/Technology.ts
@@ -0,0 +1,7 @@
+export interface Technology {
+  id: string;
+  name: string;
+  description: string;
+  documentationLink: string;
+  color: string;
+}
